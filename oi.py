24c4701f1dfad3cd601ba9dd204f0b36d881f0a6
hugoboiola <75331445+hugoboiola@users.noreply.github.com>
2021-07-13 19:08:30 -0300

    Add files via upload

---
+++ oi.py
@@ -0,0 +1,21 @@
+a = []
+b = []
+c = []
+
+# Coloca os valores na lista 'a'
+
+for i in range(10):
+    d = int(input('Coloque um numero: '))
+    a.append(d)
+print(a) 
+
+# Coloca os valores na lista 'b'
+
+for i in range(10):
+    d = int(input('Coloque um numero: '))
+    b.append(d)
+print(b) 
+# Somando as duas listas
+
+c = a -b
+print(c)
